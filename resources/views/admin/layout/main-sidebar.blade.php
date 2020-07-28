6e68a42b915fd0b9e74f8468ad1bf1de44a02e0d
Hai Duong <hai.duong@clv.local>
2020-07-28 08:35:54 +0700

    [Update UI] admin and web index

--- resources/views/admin/layout/main-sidebar.blade.php
+++ resources/views/admin/layout/main-sidebar.blade.php
@@ -1,39 +1,28 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-        <img src="{{ asset('img/AdminLTELogo.png') }}"
+        <img src="{{ asset('img/admin.png') }}"
              alt="Product Demo"
-             class="brand-image img-circle elevation-3"
-             style="opacity: .8">
-        <span class="brand-text font-weight-light">Product Demo</span>
+             class="brand-image img-circle elevation-3">
+        <span class="brand-text">Admin Home</span>
     </a>
 
-    <!-- Sidebar -->
+    <!-- Product Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset('img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Admin</a>
-            </div>
-        </div>
-
-        <!-- Sidebar Menu -->
+        <!-- Product Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="nav-icon fas fa-music"></i>
                         <p>
-                            Products
-                            <i class="right fas fa-angle-left"></i>
+                            Product
+                            <i class="right fas fa-angle-down"></i>
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
+                    <ul class="nav nav-treeview" style="padding-left:15px" style="padding-left:15px" style="padding-left:15px" style="padding-left:15px" style="padding-left:15px">
                         <li class="nav-item">
                             <a href="{{ url('admin/index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
@@ -44,6 +33,78 @@
                             <a href="{{ url('admin/productCreate') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Create a Product</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+
+    <!-- User Sidebar -->
+    <div class="sidebar">
+        <!-- User Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-user-alt"></i>
+                        <p>
+                            User
+                            <i class="right fas fa-angle-down"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview" style="padding-left:15px" style="padding-left:15px" style="padding-left:15px" style="padding-left:15px">
+                        <li class="nav-item">
+                            <a href="{{ url('admin/index') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>User List</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('admin/productCreate') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Create User</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+
+    <!-- Event Sidebar -->
+    <div class="sidebar">
+        <!-- Event Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-grin-stars"></i>
+                        <p>
+                            Event
+                            <i class="right fas fa-angle-down"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview" style="padding-left:15px" style="padding-left:15px" style="padding-left:15px">
+                        <li class="nav-item">
+                            <a href="{{ url('admin/index') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Event List</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('admin/productCreate') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Revenue & Liabilities</p>
                             </a>
                         </li>
                     </ul>
