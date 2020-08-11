bce53fab5d9a5f227428e50a0a90930d38d50308
phongnguyen1003 <nguyenxuan.phong.1989@gmail.com>
2020-08-12 00:06:30 +0700

    Phong_Update_css

--- resources/views/web/editOrder.blade.php
+++ resources/views/web/editOrder.blade.php
@@ -76,10 +76,7 @@
                     <br>
                 </div>
                 <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        {{-- <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Orders Detail</li> --}}
-                    </ol>
+                   
                 </div>
             </div>
         </div><!-- /.container-fluid -->
